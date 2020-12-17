420f2c734d7aff7f1e317d1c29168e2133b4be5a
elbeicktalat <talatelbeick@gmail.com>
2020-12-17 07:11:37 +0100

    add body app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Component } from "react";
 import Chat from "./components/Chat/Chat";
 import Sidebar from "./components/Sidebar/Sidebar";
+import "./App.scss";
 
 class App extends Component{
   render(){
-    return <div className="App">
-      <Sidebar/>
-      <Chat/>
-    </div>;
+    return <div className="app">
+      <div className="body">
+        <Sidebar/>
+        <Chat/>
+      </div>
+  </div>;
   }  
 }
 export default App;
