20ec20bc046c6343cea001ac75a994c90bf9cba9
elbeicktalat <talatelbeick@gmail.com>
2020-12-17 07:48:01 +0100

    add sidebar header

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,29 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { Chat, DonutLarge, MoreVert } from '@material-ui/icons';
+import React, { Component } from 'react';
+import "./Sidebar.scss";
+
+class Sidebar extends Component {
+  render() {
+    return <div className="Sidebar">
+      <div className="header">
+        <div className="avatar">
+          <Avatar/>
+        </div>
+        <div className="iconButtons">
+          <IconButton>
+            <DonutLarge/>
+          </IconButton>
+          <IconButton>
+            <Chat/>
+          </IconButton>
+          <IconButton>
+            <MoreVert/>
+          </IconButton>
+        </div>
+      </div>
+    </div>;
+  }
+}
+
+export default Sidebar;
