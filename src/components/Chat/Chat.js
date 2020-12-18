1b8dad630a669575ba1b2573c1d8132861dfc84b
elbeicktalat <talatelbeick@gmail.com>
2020-12-18 19:05:47 +0100

    add chat content

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import "./Chat.scss";
 import Header from './Header/Header';
+import Content from './Content/Content';
 
 class Chat extends Component {
   render() {
     return <div className="Chat">
       <Header name="Talat El Beick" lastAccess="Online"/>
+      <Content/>
     </div>;
   }
 }
---
+++ src/components/Chat/Content/Content.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Content.scss';
+
+class Content extends Component {
+  render() {
+    return <div className="Content"></div>;
+  }
+}
+
+export default Content;
